890c7d3bf56a7e10e2c1e9666a9ea5a8420f74ff
Gravewalker666 <heshanandrews99@gmail.com>
2022-07-23 17:00:05 +0530

    Update dockerfile

--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -7,7 +7,7 @@ import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.request.*
 
-fun Application.configureRouting(datasource: HikariDataSource) {
+fun Application.configureRouting(datasource: HikariDataSource? = null) {
     routing {
         get("/") {
             call.respond(HttpStatusCode.OK, "Hello World!")
