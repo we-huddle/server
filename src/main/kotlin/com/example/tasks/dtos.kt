fd343f1a10fb0391ca2a2187e15bec55e19eb9f6
YaiyaSam <kalsha.sam26@gmail.com>
2022-08-14 12:20:50 +0530

    Add endpoint to create tasks

--- src/main/kotlin/com/example/tasks/dtos.kt
+++ src/main/kotlin/com/example/tasks/dtos.kt
@@ -3,17 +3,30 @@ package com.example.tasks
 import com.example.plugins.toJsonString
 import com.wehuddle.db.enums.TaskType
 import com.wehuddle.db.tables.records.TaskRecord
+import org.jooq.JSONB
 import java.time.OffsetDateTime
 import java.util.UUID
 
+open class PartialTaskDto (
+    open val title: String,
+    open val description: String,
+    open val type: TaskType,
+    open val details: String,
+)
+
 class TaskDto (
     val id: UUID,
-    val title: String,
-    val description: String,
-    val type: TaskType,
-    val details: String,
+    override val title: String,
+    override val description: String,
+    override val type: TaskType,
+    override val details: String,
     val createdAt: OffsetDateTime,
     val updatedAt: OffsetDateTime,
+) : PartialTaskDto (
+    title,
+    description,
+    type,
+    details
 )
 
 fun TaskRecord.toDto() = TaskDto(
--- src/main/kotlin/com/example/tasks/taskRouter.kt
+++ src/main/kotlin/com/example/tasks/taskRouter.kt
@@ -1,12 +1,18 @@
 package com.example.tasks
 
+import com.example.plugins.UserPrinciple
+import com.example.plugins.toJsonB
+import com.example.plugins.toJsonString
+import com.wehuddle.db.enums.UserRole
 import com.wehuddle.db.tables.Task
 import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.call
-import io.ktor.server.auth.authenticate
+import io.ktor.server.auth.*
+import io.ktor.server.request.*
 import io.ktor.server.response.respond
 import io.ktor.server.routing.Route
 import io.ktor.server.routing.get
+import io.ktor.server.routing.post
 import io.ktor.server.routing.route
 import org.jooq.DSLContext
 
@@ -23,6 +29,24 @@ fun Route.tasks(context: DSLContext) {
                 }
                 call.respond(HttpStatusCode.OK, taskList)
             }
+
+            post {
+                val userPrinciple = call.principal<UserPrinciple>()!!
+                if (userPrinciple.profile.role == UserRole.HUDDLE_AGENT) {
+                    val taskToBeCreated = call.receive<PartialTaskDto>()
+                    val newTaskRecord = context.newRecord(TASK)
+                    newTaskRecord.title = taskToBeCreated.title
+                    newTaskRecord.description = taskToBeCreated.description
+                    newTaskRecord.type = taskToBeCreated.type
+                    newTaskRecord.details = taskToBeCreated.details.toJsonB()
+                    newTaskRecord.store()
+                } else {
+                    call.respond(HttpStatusCode.Forbidden, "Permission denied")
+                }
+
+            }
         }
+
+
     }
 }
