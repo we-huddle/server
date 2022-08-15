61fb0ac573789638fe3a5061c3791379fee21536
Heshan Andrews <45477334+Gravewalker666@users.noreply.github.com>
2022-08-15 14:02:16 +0530

    Write endpoint to get the completed tasks of the logged in huddler (#14)
    * Write completed tasks endpoint
    
    * Fix task create router

--- src/main/kotlin/com/example/tasks/taskRouter.kt
+++ src/main/kotlin/com/example/tasks/taskRouter.kt
@@ -3,7 +3,9 @@ package com.example.tasks
 import com.example.plugins.UserPrinciple
 import com.example.plugins.toJsonB
 import com.example.plugins.toJsonString
+import com.wehuddle.db.enums.AnswerStatus
 import com.wehuddle.db.enums.UserRole
+import com.wehuddle.db.tables.Answer
 import com.wehuddle.db.tables.Task
 import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.call
@@ -14,9 +16,11 @@ import io.ktor.server.routing.Route
 import io.ktor.server.routing.get
 import io.ktor.server.routing.post
 import io.ktor.server.routing.route
+import java.time.OffsetDateTime
 import org.jooq.DSLContext
 
 private val TASK = Task.TASK
+private val ANSWER = Answer.ANSWER
 
 fun Route.tasks(context: DSLContext) {
     authenticate {
@@ -39,14 +43,35 @@ fun Route.tasks(context: DSLContext) {
                     newTaskRecord.description = taskToBeCreated.description
                     newTaskRecord.type = taskToBeCreated.type
                     newTaskRecord.details = taskToBeCreated.details.toJsonB()
+                    newTaskRecord.createdAt = OffsetDateTime.now()
+                    newTaskRecord.updatedAt = OffsetDateTime.now()
                     newTaskRecord.store()
+                    call.respond(HttpStatusCode.OK)
                 } else {
                     call.respond(HttpStatusCode.Forbidden, "Permission denied")
                 }
 
             }
+
+            route("/completed") {
+                get {
+                    val userPrinciple = call.principal<UserPrinciple>()!!
+                    val result = context
+                        .select()
+                        .from(TASK)
+                        .join(ANSWER)
+                        .on(TASK.ID.eq(ANSWER.TASKID))
+                        .where(ANSWER.PROFILEID.eq(userPrinciple.profileId))
+                        .and(ANSWER.STATUS.eq(AnswerStatus.COMPLETED))
+                        .fetch()
+                        .into(Task.TASK)
+                    val taskList = mutableListOf<TaskDto>()
+                    for (record in result) {
+                        taskList.add(record.toDto())
+                    }
+                    call.respond(HttpStatusCode.OK, taskList)
+                }
+            }
         }
-
-
     }
 }
