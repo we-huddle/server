7b8f678be039c6dc395f86285d079f9d4fdbab6a
Gravewalker666 <heshanandrews99@gmail.com>
2023-02-11 12:24:07 +0530

    Change the hikari configs to set a maximum pool size

--- src/main/kotlin/com/example/plugins/Database.kt
+++ src/main/kotlin/com/example/plugins/Database.kt
@@ -4,7 +4,6 @@ import com.zaxxer.hikari.HikariConfig
 import com.zaxxer.hikari.HikariDataSource
 import org.flywaydb.core.Flyway
 import org.flywaydb.core.api.Location
-import java.util.Properties
 import javax.sql.DataSource
 
 const val ENV_DB_USER = "DB_USER"
@@ -20,16 +19,13 @@ fun configureHikariDataSource(
     port: Int = System.getenv(ENV_DB_PORT).toInt(),
     host: String = System.getenv(ENV_DB_HOST),
 ): HikariDataSource {
-    val config = HikariConfig(
-        Properties().apply {
-            setProperty("dataSourceClassName", "org.postgresql.ds.PGSimpleDataSource")
-            setProperty("dataSource.user", user)
-            setProperty("dataSource.password", password)
-            setProperty("dataSource.databaseName", dbName)
-            setProperty("dataSource.portNumber", port.toString())
-            setProperty("dataSource.serverName", host)
-        }
-    )
+    val config = HikariConfig().apply {
+        this.jdbcUrl = "jdbc:postgresql://$host:$port/$dbName"
+        this.username = user
+        this.password = password
+        this.maximumPoolSize = 20
+        this.validationTimeout = 2500
+    }
     return HikariDataSource(config)
 }
 
